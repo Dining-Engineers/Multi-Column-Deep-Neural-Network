149a9d6f401f79c64fb404667fe043eb9b84d3c9
Matteo Bruni <matteo.bruni@gmail.com>
2014-10-02 13:39:19 +0200

    insert batch size

--- multicolumn.py
+++ multicolumn.py
@@ -4,21 +4,21 @@ import theano
 from theano import tensor as T
 
 
-
-# get dataset
-dataset = CIFAR10(which_set='test',
-                         start=0,
-                         stop=10000,
-                         # gcn=1,
-                         toronto_prepro=True,
-                         axes=['c', 0, 1, 'b'])
-
-x_test = dataset.get_topological_view()
-
-# print x_test[0,:]
-
-# number of column
-n_column = 2
+#
+# # get dataset
+# dataset = CIFAR10(which_set='test',
+#                          start=0,
+#                          stop=10000,
+#                          # gcn=1,
+#                          toronto_prepro=True,
+#                          axes=['c', 0, 1, 'b'])
+#
+# x_test = dataset.get_topological_view()
+#
+# # print x_test[0,:]
+#
+# # number of column
+# n_column = 2
 
 
 
@@ -29,6 +29,7 @@ model = serial.load( model_path )
 
 X = model.get_input_space().make_theano_batch(batch_size=128)
 Y = model.fprop( X )
+
 # get prediction
 f = theano.function( [X], Y )
 
