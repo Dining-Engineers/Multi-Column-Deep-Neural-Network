3baec6e99a09e6ca8e52886fc19bd097474039bb
Matteo Bruni <matteo.bruni@gmail.com>
2014-10-02 13:36:32 +0200

    insert batch size

--- multicolumn.py
+++ multicolumn.py
@@ -10,7 +10,7 @@ dataset = CIFAR10(which_set='test',
                          start=0,
                          stop=10000,
                          # gcn=1,
-                         toronto_prepro=1,
+                         toronto_prepro=True,
                          axes=['c', 0, 1, 'b'])
 
 x_test = dataset.get_topological_view()
@@ -26,9 +26,9 @@ n_column = 2
 # get model1
 model_path = 'pkl/toronto_best.pkl'
 model = serial.load( model_path )
-X = model.get_input_space().make_theano_batch()
-Y = model.fprop( X )
 
+X = model.get_input_space().make_theano_batch(batch_size=128)
+Y = model.fprop( X )
 # get prediction
 f = theano.function( [X], Y )
 
