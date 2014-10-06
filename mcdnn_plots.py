35737c7e4919a4cf05eaf27da2cbd6f77020838b
Matteo Bruni <matteo.bruni@gmail.com>
2014-10-06 17:18:09 +0200

    graph working

--- mcdnn_plots.py
+++ mcdnn_plots.py
@@ -4,32 +4,63 @@ from sklearn.metrics import confusion_matrix
 
 
 def plot_confusion_matrix():
-    y_ground_truth = np.int64(np.genfromtxt('prediction_ground_truth.csv', delimiter=','))
-    y_gcn_predictions = np.argmax(np.genfromtxt('prediction_gcn.csv', delimiter=','), axis=1)
-    y_toronto_predictions = np.argmax(np.genfromtxt('prediction_toronto.csv', delimiter=','), axis=1)
-    y_zca_predictions = np.argmax(np.genfromtxt('prediction_zca.csv', delimiter=','), axis=1)
+    y_ground_truth = np.float64(np.genfromtxt('prediction_ground_truth.csv', delimiter=','))
+    y_gcn_predictions = np.float64(np.argmax(np.genfromtxt('prediction_gcn.csv', delimiter=','), axis=1))
+    y_toronto_predictions = np.float64(np.argmax(np.genfromtxt('prediction_toronto.csv', delimiter=','), axis=1))
+    y_zca_predictions = np.float64(np.argmax(np.genfromtxt('prediction_zca.csv', delimiter=','), axis=1))
+
+    plot_cm(y_ground_truth, y_gcn_predictions, "Single GCN")
+    plot_cm(y_ground_truth, y_toronto_predictions, "Single Toronto")
+    plot_cm(y_ground_truth, y_zca_predictions, "Single ZCA")
 
 
-    print y_ground_truth[3:10]
-    print y_gcn_predictions[3:10]
 
-    cm = confusion_matrix(y_ground_truth, y_gcn_predictions, ['airplane', 'automobile', 'bird', 'cat', 'deer',
-                            'dog', 'frog', 'horse', 'ship', 'truck'])
+def plot_cm(ytrue, ypred, name):
 
 
-    plt.matshow(cm)
-    plt.title("Cifar-10 Confusion Matrix")
-    plt.colorbar()
+
+    cm = confusion_matrix(ytrue, ypred)
+
+
+    norm_conf = []
+    for i in cm:
+        a = 0
+        tmp_arr = []
+        a = sum(i, 0)
+        for j in i:
+            tmp_arr.append(float(j)/float(a))
+        norm_conf.append(tmp_arr)
+
+    labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+    fig = plt.figure()
+    plt.title("Cifar-10 Confusion Matrix - "+name)
+
+
+    ax = fig.add_subplot(111)
+    ax.set_aspect(1)
+    res = ax.imshow(np.array(norm_conf), cmap=plt.cm.jet,
+                    interpolation='nearest')
+
+    width = len(cm)
+    height = len(cm[0])
+
+    for x in xrange(width):
+        for y in xrange(height):
+            ax.annotate(str(cm[x][y]), xy=(y, x),
+                        horizontalalignment='center',
+                        verticalalignment='center')
+
+    cb = fig.colorbar(res)
+    plt.xticks(range(width), labels, rotation=45)
+    plt.yticks(range(height), labels)
+    # plt.savefig('confusion_matrix.png', format='png')
     plt.ylabel('true labels')
     plt.xlabel('predicted labels')
-    # plt.savefig('./confusion.png', dpi=150)
     plt.show()
 
-
-
-
-
-
+    # plt.clf()
+    # # plt.savefig('./confusion.png', dpi=150)
 
 
 if __name__ == '__main__':
