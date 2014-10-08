c8ddf01498b8853c575574cf313f19b0c6940980
Matteo Bruni <matteo.bruni@gmail.com>
2014-10-08 08:13:27 +0200

    added no_fix

---
+++ run_multicolumn_complex_ALL_NOFIXED.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+THEANO_FLAGS="device=gpu,floatX=float32" python train_multicolumn_complex_2COL_GCN_TOR_NOFIXED.py 2>&1 | tee output/train_multicolumn_complex_2COL_GCN_TOR_NOFIXED.txt
+THEANO_FLAGS="device=gpu,floatX=float32" python train_multicolumn_complex_2COL_GCN_ZCA_NOFIXED.py 2>&1 | tee output/train_multicolumn_complex_2COL_GCN_ZCA_NOFIXED.txt
+THEANO_FLAGS="device=gpu,floatX=float32" python train_multicolumn_complex_2COL_ZCA_TOR_NOFIXED.py 2>&1 | tee output/train_multicolumn_complex_2COL_ZCA_TOR_NOFIXED.txt
+THEANO_FLAGS="device=gpu,floatX=float32" python train_multicolumn_3COL_complex_NOFIXED.py 2>&1 | tee output/train_multicolumn_3COL_complex_NOFIXED.txt
